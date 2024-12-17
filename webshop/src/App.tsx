0a374fef61d1528b2763019f13aaa51698705dfb
Djafny <jafnyabdad@gmail.com>
2024-12-17 17:07:36 +0100

    start: launching vite + react

---
+++ webshop/src/App.tsx
@@ -0,0 +1,15 @@
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+import './App.css'
+
+function App() {
+
+  return (
+
+    <MantineProvider>
+    </MantineProvider>
+
+  )
+}
+
+export default App
